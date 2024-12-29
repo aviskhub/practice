1e963c7e4b78baa612c1bb4f783af2b3fb55c645
Abhishek singh <abhishekdropbox01@gmail.com>
2024-12-29 16:00:37 +0530

    corrected

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -32,10 +32,10 @@ resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.cluster-vpc.id
 }
 
-resource "aws_internet_gateway_attachment" "ig-attach" {
-  internet_gateway_id = aws_internet_gateway.ig.id
-  vpc_id = aws_vpc.cluster-vpc.id
-}
+# resource "aws_internet_gateway_attachment" "ig-attach" {
+#   internet_gateway_id = aws_internet_gateway.ig.id
+#   vpc_id = aws_vpc.cluster-vpc.id
+# }
 
 resource "aws_route" "route" {
   route_table_id = aws_vpc.cluster-vpc.main_route_table_id
