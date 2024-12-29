ce322b1db25b7643058ae09db3cc8f4595104269
Abhishek singh <abhishekdropbox01@gmail.com>
2024-12-29 16:02:15 +0530

    corrected

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -39,7 +39,7 @@ resource "aws_internet_gateway" "ig" {
 
 resource "aws_route" "route" {
   route_table_id = aws_vpc.cluster-vpc.main_route_table_id
-  gateway_id = aws_internet_gateway_attachment.ig-attach.id
+  gateway_id = aws_internet_gateway.ig.id
   destination_cidr_block = "0.0.0.0/0"
 }
 # // IAM role for cluster 
